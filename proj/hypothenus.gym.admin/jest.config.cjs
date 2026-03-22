71a057348c0f44392e9ab242251ea74d4b8bb73f
fortinb <fortinb.perso@gmail.com>
2026-03-22 05:36:08 -0400

    correct test package

--- proj/hypothenus.gym.admin/jest.config.cjs
+++ proj/hypothenus.gym.admin/jest.config.cjs
@@ -1,6 +1,5 @@
 /** Jest configuration for TypeScript + React Testing Library */
 module.exports = {
-  preset: 'ts-jest',
   testEnvironment: 'jest-environment-jsdom',
   testPathIgnorePatterns: ['/node_modules/', '/.next/'],
   setupFilesAfterEnv: ['<rootDir>/jest.setup.ts'],
